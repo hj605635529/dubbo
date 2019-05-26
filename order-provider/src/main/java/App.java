e1b1c158c777ee24bff8675b1ab6e4a94e9075e1
huangjia <jia.huang@qunar.com>
2019-05-26 21:03:53 +0800

    项目搭建完成

---
+++ order-provider/src/main/java/App.java
@@ -0,0 +1,12 @@
+import com.alibaba.dubbo.container.Main;
+
+/**
+ * @author jia.huang
+ * @date 2019/5/25
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
